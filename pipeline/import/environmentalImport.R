9f7e973e8b0851dae14238aac5d25e96516d9cd9
Ron Togunov <20913113+RRTogunov@users.noreply.github.com>
2023-10-24 15:42:36 +0200

    Update environmentalImport.R
    store downloaded environmental data in temp folder (with coded information on extent/resolution) and use these if they encompass focal region/mesh.

--- pipeline/import/environmentalImport.R
+++ pipeline/import/environmentalImport.R
@@ -11,6 +11,7 @@ library(raster)
 library(terra)
 library(sf)
 library(fasterize)
+library(digest)  # create hash of raster CRS and projection for saving
 
 # Import local functions
 sapply(list.files("functions/import/environment", full.names = TRUE), source)
@@ -72,7 +73,48 @@ for (parameter in 1:length(selectedParameters)) {
     rasterisedVersion <- rast(paste0("data/external/environmentalCovariates/",focalParameter, ".tiff"))
   } else {
     dataSource <- parameters$dataSource[parameters$parameters == focalParameter]
-    source(paste0("pipeline/import/utils/defineEnvSource.R"))
+    # check if data has already been downloaded
+    raster_found <- FALSE
+    if(dir.exists(paste0("data/temp/", dataSource))){
+      ## List all files in the directory that match the parameter
+      file_list <- list.files(path = paste0("data/temp/", dataSource), 
+                              pattern = paste0(focalParameter, "_.*\\.tiff$"), 
+                              full.names = TRUE)
+      # Check files
+      for (file in file_list) {
+        rast <- rast(file)
+        if (is.subset(regionGeometry_buffer, rast)) {
+          rasterisedVersion <- rast
+          raster_found <- TRUE
+          cat("Raster encompassing region found and imported successfully!\n")
+          break
+        } 
+      }
+    } else(
+      dir.create(paste0("data/temp/", dataSource))
+    )
+    
+    if (!raster_found) {
+        # download file
+        source(paste0("pipeline/import/utils/defineEnvSource.R"))
+        # get info to save
+        info <- crs(rasterisedVersion, describe = T)
+        if(!is.na(info$authority)){
+          ext <- ext(rasterisedVersion)
+          info <- sprintf("%s%s_X%s_%s_Y%s_%s",
+                          info$authority, info$code, 
+                          ext[1], ext[2], ext[3], ext[4])
+        } else {
+          info <- digest(list(crs(rasterisedVersion), ext(rasterisedVersion)))
+        }
+        #  Construct the filename and save with raster information
+        file_path <- sprintf("data/temp/%s/%s_%s.tiff",
+                             dataSource,focalParameter, info)
+                     x <- rasterisedVersion
+        if(!file.exists(file_path)){
+          writeRaster(x, filename = file_path, overwrite = TRUE)
+        }
+      }
   }
   parameterList[[parameter]] <- rasterisedVersion
 }
