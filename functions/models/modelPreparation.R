1a816e7f3f03e3b5504ec262393b41f57e7e8ec8
Ron Togunov <20913113+RRTogunov@users.noreply.github.com>
2023-10-24 16:08:58 +0200

    Update modelPreparation.R
    - Combine species by focal group before fitting model.
    - Print species added to model
    - allow for model without environmental data (e.g., environmentalDataList = NULL)

--- functions/models/modelPreparation.R
+++ functions/models/modelPreparation.R
@@ -1,18 +1,20 @@
 
+#' @importFrom terra nlyr
+#' @importFrom sf st_sf
+#' @importFrom intSDM startWorkflow
 
 #### MODEL DATA PREPARATION ####
 
 # The following script imports our various forms of environmental data and processes them, based on the type of data
 # and other specifications related to the source.
 
-modelPreparation <- function(focalSpecies, regionGeometry, modelFolderName, environmentalDataList) {
-  
+modelPreparation <- function(focalSpecies, speciesData, regionGeometry, modelFolderName, environmentalDataList = NULL) {
   focalTaxa <- unique(focalSpecies$taxonomicGroup)
   
   workflowList <- list()
   
   # Begin running different species groups
-  for (i in 1:length(focalTaxa)) {
+  for (i in seq_along(focalTaxa)) {
     
     # Define species group to create
     focalGroup <- focalTaxa[i]
@@ -30,6 +32,28 @@ modelPreparation <- function(focalSpecies, regionGeometry, modelFolderName, envi
     })
     focalSpeciesDataRefined <- focalSpeciesDataRefined[!is.na(focalSpeciesDataRefined)]
     
+    # Combine species by functionalGroup if requested (else leave as separate)
+    # identify species with data
+    uniqueTaxaSpecies <- unique(unlist(lapply(focalSpeciesDataRefined, function(ds){
+        as.character(ds$simpleScientificName)
+      })))
+    
+    # functional groups in species with data for focal taxonomic group
+    # focalFunctionalGroups <- focalSpecies$functionalGroup[focalSpecies$species %in% uniqueTaxaSpecies &
+    #                                                       focalSpecies$taxonomicGroup %in% focalGroup]
+    focalSpeciesWithData <- focalSpecies[focalSpecies$species %in% uniqueTaxaSpecies &  # species with data
+                                         focalSpecies$taxonomicGroup %in% focalGroup,]  # and of focal taxa (in case same species in different taxa)
+    # if any species are to be modelled as functional groups
+    if(any(focalSpeciesWithData$functionalGroup != "")){
+      # update data
+      focalSpeciesDataRefined <- joinFunctionalGroups(speciesData = focalSpeciesDataRefined, 
+                           focalSpecies = focalSpeciesWithData)
+                           
+      focalGroupSpecies <- unique(unlist(lapply(focalSpeciesDataRefined, function(ds){
+          as.character(ds$simpleScientificName)
+        })))
+    }
+
     # Initialise workflow, creating folder for model result storage
     workflow <- startWorkflow(
       Projection = '+proj=utm +zone=32 +ellps=WGS84 +datum=WGS84 +units=m +no_defs',
@@ -40,7 +64,7 @@ modelPreparation <- function(focalSpecies, regionGeometry, modelFolderName, envi
     
     # Add datasets - note that for the moment this excludes the NTNU field notes and ANO,
     # the model will currently not run with these involved
-    for (l in c(1:length(focalSpeciesDataRefined))) {
+    for (l in seq_along(focalSpeciesDataRefined)) {
       dataset <- focalSpeciesDataRefined[[l]]
       
       if (nrow(dataset) < 5) next
@@ -53,10 +77,12 @@ modelPreparation <- function(focalSpecies, regionGeometry, modelFolderName, envi
                              datasetName = datasetName,
                              responseName = 'individualCount',
                              speciesName = 'simpleScientificName')
+      cat(sprintf("    - %s species added. \n", length(unique(dataset$simpleScientificName))))
     }
     
     # Add environmental characteristics
-    for (e in 1:nlyr(environmentalDataList)) {
+    env <- if(is.null(environmentalDataList)) 0 else seq(nlyr(environmentalDataList))
+    for (e in env) {
       cat(sprintf("Adding covariate '%s' to the model.\n", names(environmentalDataList)[e]))
       workflow$addCovariates(Object = environmentalDataList[[e]])
     }
